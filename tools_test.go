e3ea19538e43cfeae82ba18aaf99b9f8dabbc3d9
Alfian Tirta <alf.tirta@gmail.com>
2022-09-05 07:53:38 +0700

    added test for PushJSONToRemote method

--- tools_test.go
+++ tools_test.go
@@ -16,6 +16,37 @@ import (
 	"testing"
 )
 
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+func NewTestClient(f RoundTripFunc) *http.Client {
+	return &http.Client{Transport: f}
+}
+
+func TestTools_PushJSONToRemote(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		// Test Request Parameters
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(bytes.NewBufferString("ok")),
+			Header:     make(http.Header),
+		}
+	})
+
+	var testTools Tools
+	var foo struct {
+		Bar string `json:"bar"`
+	}
+	foo.Bar = "bar"
+
+	if _, _, err := testTools.PushJSONToRemote("http://example.com/some/path", foo, client); err != nil {
+		t.Error("failed to call remote url:", err)
+	}
+}
+
 func TestTools_RandomString(t *testing.T) {
 	var testTools Tools
 	s := testTools.RandomString(10)
