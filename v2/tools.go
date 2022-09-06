c0efcfd6bbb4b3cd6bb4f4f4c238d287985849d3
Alfian Tirta <alf.tirta@gmail.com>
2022-09-06 12:36:16 +0700

    initiate v2

---
+++ v2/tools.go
@@ -0,0 +1,327 @@
+package toolkit
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+const randomStringSource string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
+// Tools is the type used to instantiate this module.
+// Any variable of this type will have access too all the methods with the receiver *Tools.
+type Tools struct {
+	MaxFileSize        int64
+	AllowedFileTypes   []string
+	MaxJSONSize        int64
+	AllowUnknownFields bool
+}
+
+// RandomString returns a string of random characters of length n,
+// using randomStringSource as the source for the string.
+func (t *Tools) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomStringSource)
+	for i := range s {
+		p, err := rand.Prime(rand.Reader, len(r))
+		if err != nil {
+			return "RandomString Error"
+		}
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
+
+// UploadedFile is a struct used to save information about an uploaded file.
+type UploadedFile struct {
+	NewFileName      string
+	OriginalFileName string
+	FileSize         int64
+}
+
+// UploadFiles uploads one or more files to a specified directory,
+// and gives the files a random name.
+// It returns a slice containing the newly named files, the original file names,
+// the size of the files, and potentially an error.
+// If the optional last parameter is set to true, then we will not rename the files,
+// but will use the original file names.
+func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	var uploadedFiles []*UploadedFile
+	var err error
+
+	if t.MaxFileSize == 0 {
+		t.MaxFileSize = 1024 * 1024 * 1024 // 1kB * 1kB * 1kB == 1GB
+	}
+
+	// create the upload directory if it does not exist
+	if err = t.CreateDirIfNotExist(uploadDir); err != nil {
+		return nil, err
+	}
+
+	if err = r.ParseMultipartForm(t.MaxFileSize); err != nil {
+		return nil, errors.New("the uploaded file is too big")
+	}
+
+	for _, fileHeaders := range r.MultipartForm.File {
+		for _, fileHeader := range fileHeaders {
+			uploadedFiles, err = func(uploadedFiles []*UploadedFile) ([]*UploadedFile, error) {
+				var uploadedFile UploadedFile
+				inFile, err := fileHeader.Open()
+				if err != nil {
+					return nil, err
+				}
+				defer inFile.Close()
+
+				// look at the first 512 bytes of the file in order to figure out what it is
+				buff := make([]byte, 512)
+
+				// get the first 512 bytes of the file
+				if _, err = inFile.Read(buff); err != nil {
+					return nil, err
+				}
+
+				// check to see if the file type is permitted
+				allowed := false
+				fileType := http.DetectContentType(buff) // "image/jpeg" || "image/png" || "image/gif" || etc.
+
+				if len(t.AllowedFileTypes) > 0 {
+					for _, allowedFileType := range t.AllowedFileTypes {
+						if strings.EqualFold(fileType, allowedFileType) {
+							allowed = true
+							break
+						}
+					}
+				} else {
+					allowed = true
+				}
+
+				if !allowed {
+					return nil, errors.New("the uploaded file type is not permitted")
+				}
+
+				if _, err = inFile.Seek(0, 0); err != nil {
+					return nil, err
+				}
+
+				if renameFile {
+					uploadedFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25), filepath.Ext(fileHeader.Filename))
+				} else {
+					uploadedFile.NewFileName = fileHeader.Filename
+				}
+
+				uploadedFile.OriginalFileName = fileHeader.Filename
+
+				var outFile *os.File
+				defer outFile.Close()
+
+				if outFile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
+					return nil, err
+				}
+
+				if fileSize, err := io.Copy(outFile, inFile); err != nil {
+					return nil, err
+				} else {
+					uploadedFile.FileSize = fileSize
+				}
+
+				uploadedFiles = append(uploadedFiles, &uploadedFile)
+				return uploadedFiles, err
+			}(uploadedFiles)
+			if err != nil {
+				return uploadedFiles, err
+			}
+		}
+	}
+
+	return uploadedFiles, err
+}
+
+// UploadOneFile is just a convenience method that calls UploadFiles, but expects only one file to be in the upload.
+func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadedFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+	files, err := t.UploadFiles(r, uploadDir, renameFile)
+	if err != nil {
+		return nil, err
+	}
+	return files[0], err
+}
+
+// CreateDirIfNotExist creates a directory, and all necessary parents, if it does not exist.
+func (t *Tools) CreateDirIfNotExist(path string) error {
+	const mode = 0755 // fs.FileMode
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if err := os.MkdirAll(path, mode); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Slugity is a (very) simple means of creating a slug from a string.
+func (t *Tools) Slugify(s string) (string, error) {
+	if s == "" {
+		return "", errors.New("empty string not permitted")
+	}
+	re := regexp.MustCompile(`[^a-z\d]+`)
+	slug := strings.Trim(re.ReplaceAllLiteralString(strings.ToLower(s), "-"), "-")
+	if len(slug) == 0 {
+		return "", errors.New("after removing characters, slug is zero length")
+	}
+	return slug, nil
+}
+
+// DownloadStaticFile downloads a file, and tries to force the browser
+// to avoid displaying it in the browser window by setting content disposition.
+// It also allows specification of the display name.
+func (t *Tools) DownloadStaticFile(w http.ResponseWriter, r *http.Request, path, fileName, displayName string) {
+	filePath := filepath.Join(path, fileName)
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", displayName))
+	http.ServeFile(w, r, filePath)
+}
+
+// JSONResponse is the type used for sending JSON around.
+type JSONResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+// ReadJSON tries to read the body of a request and converts from json into a go data variable.
+func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	var maxBytes int64 = 1024 * 1024 // 1MB
+	if t.MaxJSONSize != 0 {
+		maxBytes = t.MaxJSONSize
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, maxBytes)
+	dec := json.NewDecoder(r.Body)
+
+	if !t.AllowUnknownFields {
+		dec.DisallowUnknownFields()
+	}
+
+	if err := dec.Decode(data); err != nil {
+		var (
+			syntaxError           *json.SyntaxError
+			unmarshalTypeError    *json.UnmarshalTypeError
+			invalidUnmarshalError *json.InvalidUnmarshalError
+		)
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		case errors.As(err, &invalidUnmarshalError):
+			return fmt.Errorf("error unmarshalling JSON: %s", err.Error())
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown fifeld")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytes)
+		default:
+			return err
+		}
+	}
+
+	if err := dec.Decode(&struct{}{}); err != io.EOF {
+		return errors.New("body must contain only one JSON value")
+	}
+
+	return nil
+}
+
+// WriteJSON takes a response status code and arbitrary data and writes json to the client.
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if _, err := w.Write(out); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ErrorJSON takes an error, and optionally a status code, and generates and sends a JSON error message.
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JSONResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return t.WriteJSON(w, statusCode, payload)
+}
+
+// PushJSONToRemote posts arbitrary data to some URL as JSON,
+// and returns the response, status code, and error if any.
+// The final parameter, client, is optional.
+// If none is specified, we use the standard http.Client.
+func (t *Tools) PushJSONToRemote(uri string, data any, client ...*http.Client) (*http.Response, int, error) {
+	// create json
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// check for custom http client
+	httpClient := &http.Client{}
+	if len(client) > 0 {
+		httpClient = client[0]
+	}
+
+	// build the request and set the header
+	request, err := http.NewRequest("POST", uri, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, 0, err
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	// call the remote uri
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer response.Body.Close()
+
+	// send response back
+	return response, response.StatusCode, nil
+}
---
+++ v2/tools_test.go
@@ -0,0 +1,450 @@
+package toolkit
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"image"
+	"image/png"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"sync"
+	"testing"
+)
+
+type RoundTripFunc func(req *http.Request) *http.Response
+
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+func NewTestClient(f RoundTripFunc) *http.Client {
+	return &http.Client{Transport: f}
+}
+
+func TestTools_PushJSONToRemote(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		// Test Request Parameters
+		return &http.Response{
+			StatusCode: http.StatusOK,
+			Body:       io.NopCloser(bytes.NewBufferString("ok")),
+			Header:     make(http.Header),
+		}
+	})
+
+	var testTools Tools
+	var foo struct {
+		Bar string `json:"bar"`
+	}
+	foo.Bar = "bar"
+
+	if _, _, err := testTools.PushJSONToRemote("http://example.com/some/path", foo, client); err != nil {
+		t.Error("failed to call remote url:", err)
+	}
+}
+
+func TestTools_RandomString(t *testing.T) {
+	var testTools Tools
+	s := testTools.RandomString(10)
+	if len(s) != 10 {
+		t.Error("wrong length of random string")
+	}
+}
+
+var uploadTests = []struct {
+	name          string
+	allowedTypes  []string
+	renameFile    bool
+	errorExpected bool
+}{
+	{
+		name:          "allowed no rename",
+		allowedTypes:  []string{"image/jpeg", "image/png"},
+		renameFile:    false,
+		errorExpected: false,
+	},
+	{
+		name:          "allowed rename",
+		allowedTypes:  []string{"image/jpeg", "image/png"},
+		renameFile:    true,
+		errorExpected: false,
+	},
+	{
+		name:          "not allowed",
+		allowedTypes:  []string{"image/jpeg"},
+		renameFile:    false,
+		errorExpected: true,
+	},
+}
+
+func TestTools_UploadFiles(t *testing.T) {
+	for _, uploadTest := range uploadTests {
+		// set up a pipe to avoid buffering
+		pr, pw := io.Pipe() // *PipeReader, *PipeWriter
+		writer := multipart.NewWriter(pw)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			defer writer.Close()
+
+			// create the form data field, let's say "pngFile"
+			pngFileWriter, err := writer.CreateFormFile("pngFile", "./testdata/img.png")
+			if err != nil {
+				t.Error("error creating form file", err)
+			}
+
+			pngFileReader, err := os.Open("./testdata/img.png")
+			if err != nil {
+				t.Error("error opening img.png file", err)
+			}
+			defer pngFileReader.Close()
+
+			img, _, err := image.Decode(pngFileReader)
+			if err != nil {
+				t.Error("error decoding image", err)
+			}
+
+			if err = png.Encode(pngFileWriter, img); err != nil {
+				t.Error("error encoding png")
+			}
+		}()
+
+		// read from the pipe which receives data
+		request := httptest.NewRequest("POST", "/", pr)
+		request.Header.Add("Content-Type", writer.FormDataContentType())
+
+		var testTools Tools
+		testTools.AllowedFileTypes = uploadTest.allowedTypes
+
+		uploadedFiles, err := testTools.UploadFiles(request, "./testdata/uploads", uploadTest.renameFile)
+		if err != nil && !uploadTest.errorExpected {
+			t.Error("error uploading files", err)
+		}
+
+		if !uploadTest.errorExpected {
+			if _, err = os.Stat(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName)); os.IsNotExist(err) {
+				t.Errorf("%s: expected file to exist: %s", uploadTest.name, err.Error())
+			}
+
+			// clean up
+			if err = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName)); err != nil {
+				t.Error("error removing file", err)
+			}
+		}
+
+		if !uploadTest.errorExpected && err != nil {
+			t.Errorf("%s: error expected but none received", uploadTest.name)
+		}
+
+		wg.Wait()
+	}
+}
+
+func TestTools_UploadOneFile(t *testing.T) {
+	// set up a pipe to avoid buffering
+	pr, pw := io.Pipe()
+	writer := multipart.NewWriter(pw)
+
+	go func() {
+		defer writer.Close()
+
+		// create the form data field, let's say "pngFile"
+		pngFileWriter, err := writer.CreateFormFile("pngFile", "./testdata/img.png")
+		if err != nil {
+			t.Error("error creating form file", err)
+		}
+
+		pngFileReader, err := os.Open("./testdata/img.png")
+		if err != nil {
+			t.Error("error opening img.png file", err)
+		}
+		defer pngFileReader.Close()
+
+		img, _, err := image.Decode(pngFileReader)
+		if err != nil {
+			t.Error("error decoding image", err)
+		}
+
+		if err = png.Encode(pngFileWriter, img); err != nil {
+			t.Error("error encoding png")
+		}
+	}()
+
+	// read from the pipe which receives data
+	request := httptest.NewRequest("POST", "/", pr)
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+
+	var testTools Tools
+
+	uploadedFile, err := testTools.UploadOneFile(request, "./testdata/uploads")
+	if err != nil {
+		t.Error("error uploading files", err)
+	}
+
+	if _, err = os.Stat(fmt.Sprintf("./testdata/uploads/%s", uploadedFile.NewFileName)); os.IsNotExist(err) {
+		t.Errorf("expected file to exist: %s", err.Error())
+	}
+
+	// clean up
+	if err = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFile.NewFileName)); err != nil {
+		t.Error("error removing file", err)
+	}
+}
+
+func TestTools_CreateDirIfNotExist(t *testing.T) {
+	var testTool Tools
+
+	// test when the directory does not exist
+	if err := testTool.CreateDirIfNotExist("./testdata/testdir"); err != nil {
+		t.Error(err)
+	}
+
+	// test when the directory does exist
+	if err := testTool.CreateDirIfNotExist("./testdata/testdir"); err != nil {
+		t.Error(err)
+	}
+
+	if err := os.Remove("./testdata/testdir"); err != nil {
+		t.Error(err)
+	}
+}
+
+var slugTests = []struct {
+	name          string
+	s             string
+	expected      string
+	errorExpected bool
+}{
+	{
+		name:          "valid string",
+		s:             "now is the time",
+		expected:      "now-is-the-time",
+		errorExpected: false,
+	},
+	{
+		name:          "empty string",
+		s:             "",
+		expected:      "",
+		errorExpected: true,
+	},
+	{
+		name:          "complex string",
+		s:             "Now is the time for all GOOD men! + fish & such &^123",
+		expected:      "now-is-the-time-for-all-good-men-fish-such-123",
+		errorExpected: false,
+	},
+	{
+		name:          "japanese string",
+		s:             "こんにちは世界",
+		expected:      "",
+		errorExpected: true,
+	},
+	{
+		name:          "japanese string and roman characters",
+		s:             "hello world こんにちは世界",
+		expected:      "hello-world",
+		errorExpected: false,
+	},
+}
+
+func TestTools_Slugify(t *testing.T) {
+	var testTool Tools
+	for _, test := range slugTests {
+		slugified, err := testTool.Slugify(test.s)
+		if err != nil && !test.errorExpected {
+			t.Errorf("%s: error received when none expected: %s", test.name, err.Error())
+		}
+		if !test.errorExpected && slugified != test.expected {
+			t.Errorf("%s: wrong slug returned; expected %s but got %s", test.name, test.expected, slugified)
+		}
+	}
+}
+
+func TestTools_DownloadStaticFile(t *testing.T) {
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/arbitrary-endpoint", nil)
+
+	var testTool Tools
+	testTool.DownloadStaticFile(rec, req, "./testdata", "pic.jpg", "puppy.jpg")
+
+	res := rec.Result()
+	defer res.Body.Close()
+
+	if res.Header["Content-Length"][0] != "98827" {
+		t.Error("wrong content length of", res.Header["Content-Length"][0])
+	}
+
+	if res.Header["Content-Disposition"][0] != "attachment; filename=\"puppy.jpg\"" {
+		t.Error("wrong content disposition")
+	}
+
+	if _, err := io.ReadAll(res.Body); err != nil {
+		t.Error(err)
+	}
+}
+
+var readJSONTests = []struct {
+	name               string
+	json               string
+	errorExpected      bool
+	maxJSONSize        int64
+	allowUnknownFields bool
+}{
+	{
+		name:               "good json",
+		json:               `{"foo": "bar"}`,
+		errorExpected:      false,
+		maxJSONSize:        1024,
+		allowUnknownFields: false,
+	},
+	{
+		name:               "badly formatted json",
+		json:               `{"foo":}`,
+		errorExpected:      true,
+		maxJSONSize:        1024,
+		allowUnknownFields: false,
+	},
+	{
+		name:               "incorrect type",
+		json:               `{"foo": 1}`,
+		errorExpected:      true,
+		maxJSONSize:        1024,
+		allowUnknownFields: false,
+	},
+	{
+		name:               "multiple json data",
+		json:               `[{"foo": "1"}{"alpha": "beta"}]`,
+		errorExpected:      true,
+		maxJSONSize:        1024,
+		allowUnknownFields: false,
+	},
+	{
+		name:               "empty body",
+		json:               ``,
+		errorExpected:      true,
+		maxJSONSize:        1024,
+		allowUnknownFields: false,
+	},
+	{
+		name:               "syntax error in json",
+		json:               `{"foo": 1"`,
+		errorExpected:      true,
+		maxJSONSize:        1024,
+		allowUnknownFields: false,
+	},
+	{
+		name:               "unknown field in json",
+		json:               `{"fooo": "bar"}`,
+		errorExpected:      true,
+		maxJSONSize:        1024,
+		allowUnknownFields: false,
+	},
+	{
+		name:               "allow unknown fields in json",
+		json:               `{"fooo": "bar"}`,
+		errorExpected:      false,
+		maxJSONSize:        1024,
+		allowUnknownFields: true,
+	},
+	{
+		name:               "missing field name",
+		json:               `{jack: "bar"}`,
+		errorExpected:      true,
+		maxJSONSize:        1024,
+		allowUnknownFields: true,
+	},
+	{
+		name:               "file too large",
+		json:               `{"foo": "bar"}`,
+		errorExpected:      true,
+		maxJSONSize:        5,
+		allowUnknownFields: true,
+	},
+	{
+		name:               "not json",
+		json:               `Hello, World!`,
+		errorExpected:      true,
+		maxJSONSize:        1024,
+		allowUnknownFields: true,
+	},
+}
+
+func TestTools_ReadJSON(t *testing.T) {
+	var testTool Tools
+	for _, test := range readJSONTests {
+		// set the max file size
+		testTool.MaxJSONSize = test.maxJSONSize
+
+		// allow/disallow unknown fields
+		testTool.AllowUnknownFields = test.allowUnknownFields
+
+		// declare a variable to read the decoded json into
+		var decodedJSON struct {
+			Foo string `json:"foo"`
+		}
+
+		// create a request with the body
+		req := httptest.NewRequest("POST", "/arbitrary-endpoint", bytes.NewReader([]byte(test.json)))
+		defer req.Body.Close()
+
+		// create a recorder
+		rr := httptest.NewRecorder()
+
+		// test the ReadJSON method
+		err := testTool.ReadJSON(rr, req, &decodedJSON)
+		if test.errorExpected && err == nil {
+			t.Errorf("%s: error expected, but none received", test.name)
+		}
+		if !test.errorExpected && err != nil {
+			t.Errorf("%s: error not expected, but one received: %s", test.name, err.Error())
+		}
+	}
+}
+
+func TestTools_WriteJSON(t *testing.T) {
+	var testTools Tools
+
+	rr := httptest.NewRecorder()
+
+	payload := JSONResponse{
+		Error:   false,
+		Message: "foo",
+	}
+
+	headers := make(http.Header)
+	headers.Add("FOO", "BAR")
+
+	if err := testTools.WriteJSON(rr, http.StatusOK, payload, headers); err != nil {
+		t.Errorf("failed to write JSON: %v", err)
+	}
+}
+
+func TestTools_ErrorJSON(t *testing.T) {
+	var testTools Tools
+
+	rr := httptest.NewRecorder()
+	if err := testTools.ErrorJSON(rr, errors.New("some error"), http.StatusServiceUnavailable); err != nil {
+		t.Error(err)
+	}
+
+	var payload JSONResponse
+	decoder := json.NewDecoder(rr.Body)
+	if err := decoder.Decode(&payload); err != nil {
+		t.Error("received error when decoding JSON", err)
+	}
+
+	if !payload.Error {
+		t.Error("error set to false in JSON, and it should be true")
+	}
+
+	if rr.Code != http.StatusServiceUnavailable {
+		t.Errorf("wrong status code returned; expected 503, but got %d", rr.Code)
+	}
+}
